858be7cce53df774567eb92d3a71efc30135f6b3
JanithaDissanayaka <janithadissnayakaa@gmail.com>
2025-12-15 21:18:34 +0530

    add security group

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable vpc_cidr_blocks {}
 variable subnet_cidr_blocks {}
 variable available_zone {}
 variable env_prefix {}
+variable my_ip {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_blocks
@@ -27,6 +28,14 @@ resource "aws_subnet" "myapp-subnet-1" {
   
 }
 
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name="${var.env_prefix}-igw"
+  }
+  
+}
+
 /*resource "aws_route_table" "myapp-route-table" {
   vpc_id = aws_vpc.myapp-vpc.id
 
@@ -40,14 +49,6 @@ resource "aws_subnet" "myapp-subnet-1" {
   }
 
 }*/
-
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name="${var.env_prefix}-igw"
-  }
-  
-}
 
 /*resource "aws_route_table_association" "art-subnet" {
    subnet_id = aws_subnet.myapp-subnet-1.id
@@ -64,7 +65,69 @@ resource "aws_default_route_table" "default-rtb" {
   }
 
   tags = {
-    Name="${var.env_prefix}-rtb"
+    Name="${var.env_prefix}-main-rtb"
+  }
+  
+}
+
+/*
+
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress{
+    from_port=22
+    to_port=22
+    protocol="tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  
+  ingress{
+    from_port=80
+    to_port=80
+    protocol="tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress{
+    from_port=0
+    to_port=0
+    protocol="-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  
+} */
+
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress{
+    from_port=22
+    to_port=22
+    protocol="tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  
+  ingress{
+    from_port=80
+    to_port=80
+    protocol="tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress{
+    from_port=0
+    to_port=0
+    protocol="-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+     Name="${var.env_prefix}-default-sg"
   }
   
 }
